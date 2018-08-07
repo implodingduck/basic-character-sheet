d8b4a835227cbcff03525770e7626b09f292c171
implodingduck <implodingduck@users.noreply.github.com>
2018-08-06 19:55:41 -0500

    moving to a docs directory

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const server = require('http').createServer(app);
 
 
-app.use(express.static(__dirname + '/gh-pages/'));
+app.use(express.static(__dirname + '/docs/'));
 app.get('/', function(req, res,next) {  
-    res.sendFile(__dirname + '/gh-pages/index.html');
+    res.sendFile(__dirname + '/docs/index.html');
 });
 
 

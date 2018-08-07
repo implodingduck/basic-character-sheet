68a0e1ff8f2568f908f584107f6aeae9d617a0ee
implodingduck <implodingduck@users.noreply.github.com>
2018-08-06 19:54:03 -0500

    moving to gh-pages

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const server = require('http').createServer(app);
 
 
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/gh-pages/'));
 app.get('/', function(req, res,next) {  
-    res.sendFile(__dirname + '/dist/index.html');
+    res.sendFile(__dirname + '/gh-pages/index.html');
 });
 
 
